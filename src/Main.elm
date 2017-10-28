c94451f63134c37e10b761c4ca69921dbb4de1c4
Thomas Weiser <git@thomasweiser.de>
2017-10-28 14:21:01 +0200

    Start off using package jamesmacaulay/elm-graphql

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,11 @@ import Html exposing (Html)
 import Html.Attributes
 import Html.Events
 import Http
+import Task
+import GraphQL.Request.Builder as GB
+import GraphQL.Request.Builder.Arg as GBArg
+import GraphQL.Request.Builder.Variable as GBVar
+import GraphQL.Client.Http as GraphQLClient
 
 
 type alias Post =
@@ -15,6 +20,35 @@ type alias Post =
 
 type alias Posts =
     List Post
+
+
+queryPostByNodeId : GB.Document GB.Query Post { vars | nodeId : String }
+queryPostByNodeId =
+    let
+        nodeIdVar =
+            GBVar.required "nodeId" .nodeId GBVar.id
+
+        post =
+            GB.object Post
+                |> GB.with (GB.field "id" [] GB.int)
+                |> GB.with (GB.field "summary" [] (GB.nullable GB.string))
+
+        queryRoot =
+            GB.extract <|
+                (GB.field "post"
+                    [ ( "nodeId", GBArg.variable nodeIdVar ) ]
+                    post
+                )
+    in
+        GB.queryDocument queryRoot
+
+
+sendQuery1 : Cmd Msg
+sendQuery1 =
+    queryPostByNodeId
+        |> GB.request { nodeId = "WyJwb3N0cyIsMl0=" }
+        |> GraphQLClient.sendQuery "http://localhost:5000/graphql"
+        |> Task.attempt QueryResult
 
 
 main : Program Never Model Msg
@@ -30,20 +64,23 @@ main =
 type alias Model =
     { searchString : String
     , posts : Posts
+    , singlePost : Maybe Post
     }
 
 
 type Msg
     = SearchString String
     | ApiResult (Result Http.Error Posts)
+    | QueryResult (Result GraphQLClient.Error Post)
 
 
 init : ( Model, Cmd Msg )
 init =
     ( { posts = []
       , searchString = ""
+      , singlePost = Nothing
       }
-    , getSearchPosts ""
+    , Cmd.batch [ getSearchPosts "", sendQuery1 ]
     )
 
 
@@ -67,11 +104,24 @@ update msg model =
             in
                 ( model, Cmd.none )
 
+        QueryResult (Err graphQLClientError) ->
+            let
+                _ =
+                    Debug.log "QueryResult GraphQLClient Error" graphQLClientError
+            in
+                ( model, Cmd.none )
+
+        QueryResult (Ok post) ->
+            ( { model | singlePost = Just post }
+            , Cmd.none
+            )
+
 
 view : Model -> Html Msg
 view model =
     Html.div []
-        [ Html.input
+        [ Html.div [] [ Html.text (toString model.singlePost) ]
+        , Html.input
             [ Html.Attributes.placeholder "Search within posts"
             , Html.Events.onInput SearchString
             ]
@@ -95,14 +145,20 @@ getSearchPosts : String -> Cmd Msg
 getSearchPosts searchString =
     Http.post
         "http://localhost:5000/graphql"
-        (Http.stringBody "application/graphql" ("query { searchPosts(search: \"" ++ searchString ++ "\") { nodes { id summary } } }"))
+        (Http.stringBody
+            "application/graphql"
+            ("query { searchPosts(search: \""
+                ++ searchString
+                ++ "\") { nodes { id summary } } }"
+            )
+        )
         (Json.Decode.at [ "data", "searchPosts", "nodes" ] decoderListOfPosts)
         |> Http.send ApiResult
 
 
 decoderListOfPosts : Decoder Posts
 decoderListOfPosts =
-        Json.Decode.list <|
-            Json.Decode.map2 Post
-                (Json.Decode.field "id" Json.Decode.int)
-                (Json.Decode.field "summary" <| Json.Decode.maybe Json.Decode.string)
+    Json.Decode.list <|
+        Json.Decode.map2 Post
+            (Json.Decode.field "id" Json.Decode.int)
+            (Json.Decode.field "summary" <| Json.Decode.maybe Json.Decode.string)
